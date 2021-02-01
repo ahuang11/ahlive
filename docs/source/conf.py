2df1a74e04b61b685b554b25b339b6b7f469cd71
ahuang11 <ahuang11@illinois.edu>
2021-01-31 20:14:50 -0600

    Add ipykernel to list of doc reqs and revert back to python3 env

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,5 +57,5 @@ html_static_path = ["_static"]
 nbsphinx_allow_errors = True
 
 # Use this kernel instead of the one stored in the notebook metadata:
-nbsphinx_kernel_name = 'latest'
+nbsphinx_kernel_name = 'python3'
 
