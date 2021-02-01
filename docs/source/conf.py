70e21a2cdd910307acca2085cb5e585f620563f5
ahuang11 <ahuang11@illinois.edu>
2021-01-31 19:49:05 -0600

    Change default kernel

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,3 +55,8 @@ html_static_path = ["_static"]
 
 # allow errors
 nbsphinx_allow_errors = True
+
+
+# Use this kernel instead of the one stored in the notebook metadata:
+nbsphinx_kernel_name = 'python'
+
